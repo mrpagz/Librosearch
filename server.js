6f686370bd348ce1fd9bf333e3988e9e10e042ca
Francis Roy Balasabas <misterpagz@MisterpagZ.local>
2021-02-28 15:41:29 -0500

    addedroutes folder

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/googleBookSearchdb"
+  process.env.MONGODB_URI || "mongodb://localhost/googleBookdb"
 );
 
 // Start the API server
