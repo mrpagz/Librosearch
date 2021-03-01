2cb753ee7870451489c9e016546b1f0db61aeae1
Francis Roy Balasabas <misterpagz@MisterpagZ.local>
2021-02-28 21:03:43 -0500

    deployment to heroku

---
+++ client/build/precache-manifest.67a0e1dd073056b943d5df26c5064cce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2ab748cea089532700e541b2dbf9f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e7f404250dcf53229a4",
+    "url": "/static/css/main.268a39f6.chunk.css"
+  },
+  {
+    "revision": "9dafc2be3e2510d2f94d",
+    "url": "/static/js/2.80851e6f.chunk.js"
+  },
+  {
+    "revision": "9be8f09f0bfe1d20164d4c42e389b466",
+    "url": "/static/js/2.80851e6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e7f404250dcf53229a4",
+    "url": "/static/js/main.c6b78c3a.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
